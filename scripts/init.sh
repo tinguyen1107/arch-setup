5e3cfd773c8cee252c685d3c46704969b070f790
tinguyen <ntrongtin11702@gmail.com>
2024-07-20 14:32:15 +0700

    fix(package): add more package
    - zsh config
    - docker stuffs

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-sudo pacman -Sy --noconfirm neovim tmux openssl make zsh neofetch
+sudo pacman -Sy --noconfirm \
+  neovim \
+  tmux \
+  openssl \
+  make \
+  zsh \
+  zsh-syntax-highlighting \
+  docker \
+  docker-compose \
+  neofetch
 
 # set up oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
