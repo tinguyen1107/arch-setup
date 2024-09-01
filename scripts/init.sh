399c53dd77fe6b516667e78990b5e7b4a0291a4f
tinguyen <ntrongtin11702@gmail.com>
2024-09-01 09:48:10 +0700

    fix(init): install + setup docker

--- scripts/init.sh
+++ scripts/init.sh
@@ -19,3 +19,15 @@ sudo pacman -Sy --noconfirm \
 
 # set up oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# setup docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service 
+
+if groups $USER | grep -q "\bdocker\b"; then
+  echo "$USER is already in the docker group"
+else
+  echo "Adding $USER to the docker group"
+  sudo usermod -aG docker $USER
+  echo "$USER has been added to the docker group. Please log out and log back in for the changes to take effect."
+fi
